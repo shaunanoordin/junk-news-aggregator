f757ab6f88fe84736077535ac9d3ceb7e39f8608
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-05-21 23:43:30 +0100

    WIP on webpack

--- app/main.js
+++ app/main.js
@@ -71,15 +71,14 @@
 /************************************************************************/
 /******/ ({
 
-/***/ "./src/importExample.js":
-/*!******************************!*\
-  !*** ./src/importExample.js ***!
-  \******************************/
+/***/ "./src/assets/simple-bg.png":
+/*!**********************************!*\
+  !*** ./src/assets/simple-bg.png ***!
+  \**********************************/
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-"use strict";
-eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n\nvar _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if (\"value\" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } }\n\n/*  Example Class\n    Demonstrates ES6 import function in main.js\n */\n//==============================================================================\nvar ImportExample = exports.ImportExample = function () {\n  function ImportExample() {\n    var txt = arguments.length > 0 && arguments[0] !== undefined ? arguments[0] : \"DEFAULT TEXT\";\n\n    _classCallCheck(this, ImportExample);\n\n    this.txt = txt;\n  }\n\n  _createClass(ImportExample, [{\n    key: \"getText\",\n    value: function getText() {\n      return \"An ImportExample class was imported; it has the text: \" + this.txt;\n    }\n  }]);\n\n  return ImportExample;\n}();\n//==============================================================================\n\n//# sourceURL=webpack:///./src/importExample.js?");
+eval("module.exports = __webpack_require__.p + \"src/assets/simple-bg.png\";\n\n//# sourceURL=webpack:///./src/assets/simple-bg.png?");
 
 /***/ }),
 
@@ -91,7 +90,18 @@ eval("\n\nObject.defineProperty(exports, \"__esModule\", {\n  value: true\n});\n
 /***/ (function(module, exports, __webpack_require__) {
 
 "use strict";
-eval("\n\nvar _importExample = __webpack_require__(/*! ./importExample.js */ \"./src/importExample.js\");\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } } /*  \n                                                                                                                                                          Starter JS\n                                                                                                                                                          ----------\n                                                                                                                                                          \n                                                                                                                                                          Starter template for JS projects.\n                                                                                                                                                          \n                                                                                                                                                          (Shaun A. Noordin | shaunanoordin.com | 20180413)\n                                                                                                                                                           */\n\n/*  Primary App Class\n */\n//==============================================================================\nvar App = function App() {\n  _classCallCheck(this, App);\n\n  var importExample = new _importExample.ImportExample(\"HI THERE\");\n\n  this.console = document.getElementById(\"console\");\n  this.console.innerHTML = \"This is a starter template for JS projects. <br>\" + importExample.getText();\n};\n//==============================================================================\n\n/*  Initialisations\n */\n//==============================================================================\n\n\nvar app;\nwindow.onload = function () {\n  window.app = new App();\n};\n//==============================================================================\n\n//# sourceURL=webpack:///./src/main.js?");
+eval("\n\n__webpack_require__(/*! ./main.styl */ \"./src/main.styl\");\n\n__webpack_require__(/*! ./assets/simple-bg.png */ \"./src/assets/simple-bg.png\");\n\nfunction _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError(\"Cannot call a class as a function\"); } } /*  \n                                                                                                                                                          Junk News Aggregator\n                                                                                                                                                          --------------------\n                                                                                                                                                          \n                                                                                                                                                          (Shaun A. Noordin | shaunanoordin.com | 20180518)\n                                                                                                                                                          --------------------------------------------------------------------------------\n                                                                                                                                                           */\n\n//Import style and asset files, so Webpack knows to package them.\n\n\n/*  Primary App Class\n */\n//==============================================================================\nvar App = function App() {\n  _classCallCheck(this, App);\n\n  this.console = document.getElementById(\"console\");\n};\n//==============================================================================\n\n/*  Initialisations\n */\n//==============================================================================\n\n\nvar app;\nwindow.onload = function () {\n  window.app = new App();\n};\n//==============================================================================\n\n//# sourceURL=webpack:///./src/main.js?");
+
+/***/ }),
+
+/***/ "./src/main.styl":
+/*!***********************!*\
+  !*** ./src/main.styl ***!
+  \***********************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+eval("// removed by extract-text-webpack-plugin\n\n//# sourceURL=webpack:///./src/main.styl?");
 
 /***/ })
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,54 @@
-var path = require('path');
+var path = require("path");
+var nib = require("nib");
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 module.exports = {
   entry: "./src/main.js",
+  
   output: {
     filename: "[name].js",
-    path: path.resolve(__dirname, 'app'),
+    path: path.resolve(__dirname, "app"),
+    //publicPath: "/",
   },
+  
+  plugins: [
+    new ExtractTextPlugin({
+      filename: '[name].css',
+      allChunks: true,
+    }),
+  ],
+  
   mode: "development",
+  
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /node_modules/,
         use: "babel-loader"
-      }
+      },
+      {
+        test: /\.(jpg|png|gif|svg\d?)$/,
+        exclude: /node_modules/,
+        use: "file-loader?name=[path][name].[ext]"
+      },
+      {
+        test: /\.styl$/,
+        use: ExtractTextPlugin.extract({
+          fallback: 'style-loader',
+          use: [
+            {
+              loader: 'css-loader',
+            },
+            {
+              loader: 'stylus-loader',
+              options: {
+                use: [nib()],
+              },
+            }
+          ],
+        }),
+      },
     ]
   },
 }
