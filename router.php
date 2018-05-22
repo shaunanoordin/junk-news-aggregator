671033303e2d1252000769d4e62e9372c0521ea7
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-05-22 23:00:00 +0100

    Add documentation for router.php

--- router.php
+++ router.php
@@ -1,62 +1,31 @@
 <?php
-$fileURI = dirname(__FILE__) . $_SERVER['REQUEST_URI'];
+/*
+Router for PHP's Built-In Web Server
+------------------------------------
 
-if (preg_match('/\.(?:html|txt|js|ico|css|png|jpg|jpeg|gif)$/', $_SERVER["REQUEST_URI"])) {
-  return false;    // serve the requested resource as-is.
-} else { 
-  include_once $fileURI;
+PHP5 has a built-in webserver that allows us to test .PHP files without having
+to setup Apache or WAMP or similar. Sweet! This router.php provides the basic
+rules necessary for the built-in webserver to recognise which files to serve as
+a PHP resource (i.e. pages/scripts to process) and which files to serve as
+standard asset resources (e.g. image files)
+
+Usage:
+  php -S localhost:3000 router.php
+
+NOTE: in practice, PHP's built-in web server and this router file is ONLY used
+in a development environment.
+
+--------------------------------------------------------------------------------
+ */
+
+$fileURI = dirname(__FILE__) . $_SERVER["REQUEST_URI"];
+
+if (preg_match("/\.(?:html|txt|js|ico|css|png|jpg|jpeg|gif)$/i", $_SERVER["REQUEST_URI"])) {
+  return false;  //Serve the requested resource as a standard file.
+} else {
+  include_once $fileURI;  //Serve the file as a PHP resource.
 }
 
-
-/*$indexFiles = ['index.html', 'index.php'];
-$routes = [
-  '^/api(/.*)?$' => '/index.php'
-];
-
-$requestedAbsoluteFile = dirname(__FILE__) . $_SERVER['REQUEST_URI'];
-
-// check if the the request matches one of the defined routes
-foreach ($routes as $regex => $fn)
-{
-  if (preg_match('%'.$regex.'%', $_SERVER['REQUEST_URI']))
-  {
-    $requestedAbsoluteFile = dirname(__FILE__) . $fn;
-    break;
-  }
-}
-
-// if request is a directory call check if index files exist
-if (is_dir($requestedAbsoluteFile))
-{
-  foreach ($indexFiles as $filename)
-  {
-    $fn = $requestedAbsoluteFile.'/'.$filename;
-    if (is_file($fn))
-    {
-      $requestedAbsoluteFile = $fn;
-      break;
-    }
-  }
-}
-
-// if requested file does not exist or is directory => 404
-if (!is_file($requestedAbsoluteFile))
-{
-  header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found');
-  printf('"%s" does not exist', $_SERVER['REQUEST_URI']);
-  return true;
-}
-
-// if requested file is'nt a php file
-if (!preg_match('/\.php$/', $requestedAbsoluteFile)) {
-  header('Content-Type: '.mime_content_type($requestedAbsoluteFile));
-  $fh = fopen($requestedAbsoluteFile, 'r');
-  fpassthru($fh);
-  fclose($fh);
-  return true;
-}
-
-// if requested file is php, include it
-include_once $requestedAbsoluteFile;
-*/
+//TODO: handle 404s.
+//TODO: use index.php as the default file.
 ?>
