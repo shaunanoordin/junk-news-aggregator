5f4f5d08e482bfecd755daae26ae3a6b247f3f35
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-06-03 23:55:54 +0100

    API functionality

---
+++ api/config.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Config
+------
+
+Update the content of this file with the actual connection data used by the live
+server.
+
+--------------------------------------------------------------------------------
+ */
+
+$db_server = "localhost";
+$db_username = "username";
+$db_password = "password";
+$db_database = "database_name";
+$db_table = "database_main_table";
+
+?>
