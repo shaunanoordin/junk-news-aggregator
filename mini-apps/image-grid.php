4f05613a857910a2c5138d0284486684204ba1ba
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2018-10-29 23:00:11 +0000

    Add OII footer to mini-apps

--- mini-apps/image-grid.php
+++ mini-apps/image-grid.php
@@ -181,6 +181,7 @@ function print_json($data) {
 <?php if ($config->imageGridMiniApp->footer && strlen(trim($config->imageGridMiniApp->footer)) > 0) { ?>
 <footer><?= $config->imageGridMiniApp->footer ?></footer>
 <?php } ?>
+<div class="oii-footer">&copy; <a href="http://www.oii.ox.ac.uk/">Oxford Internet Institute</a> 2018 | <a href="http://www.oii.ox.ac.uk/legal/">Terms of Use</a> | <a href="http://www.oii.ox.ac.uk/legal/privacy.cfm">Privacy Policy</a> | <a href="https://www.oii.ox.ac.uk/cookie-statement/">Cookie Statement</a> | <a href="http://www.oii.ox.ac.uk/legal/copyright.cfm">Copyright Policy</a> | <a href="http://www.oii.ox.ac.uk/legal/accessibility.cfm">Accessibility</a></div>
 </div><!--/#app-->
 </body>
 </html>
