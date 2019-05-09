cf35c6f1645a4ca1210c542087c2e965b26e0a7e
Shaun A. Noordin <shaun.a.noordin@gmail.com>
2019-05-09 23:37:19 +0100

    Reset /app

--- app/common/footer.php
+++
@@ -1,7 +0,0 @@
-<?php if ($config->mainApp->footer && strlen(trim($config->mainApp->footer)) > 0) { ?>
-<footer><?= $config->mainApp->footer ?></footer>
-<?php } ?>
-<div class="oii-footer">&copy; <a href="http://www.oii.ox.ac.uk/">Oxford Internet Institute</a> 2018 | <a href="http://www.oii.ox.ac.uk/legal/">Terms of Use</a> | <a href="http://www.oii.ox.ac.uk/legal/privacy.cfm">Privacy Policy</a> | <a href="https://www.oii.ox.ac.uk/cookie-statement/">Cookie Statement</a> | <a href="http://www.oii.ox.ac.uk/legal/copyright.cfm">Copyright Policy</a> | <a href="http://www.oii.ox.ac.uk/legal/accessibility.cfm">Accessibility</a></div>
-</div><!--/#app-->
-</body>
-</html>
